b68b3786c967e055db6c49a7dbb01160021cb2b8
Kenny Pan <kenny.pan@adlinktech.com>
2019-06-20 11:17:05 +0800

    Boost IPC
    Add boost IPC sample by using shared-memory

---
+++ BoostIPCClient.cpp
@@ -0,0 +1,39 @@
+﻿// BoostIPCClient.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <boost/interprocess/shared_memory_object.hpp>
+#include <boost/interprocess/mapped_region.hpp>
+
+
+int main()
+{
+    std::cout << "IPC Client start!\n"; 
+	using namespace boost::interprocess;
+	try {
+		shared_memory_object shdmem{ open_or_create, "Boost", read_write };
+		
+		mapped_region region{ shdmem, read_write };
+
+		std::string payload(static_cast<const char *>(region.get_address()), region.get_size());
+		
+		std::cout << payload << '\n';
+
+		shared_memory_object::remove("Boost");
+		std::cout << "Shared object deletetd" << std::endl;
+	}
+	catch (interprocess_exception& e) {
+		// .. .  clean up 
+	}
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ BoostIPCServer.cpp
@@ -0,0 +1,29 @@
+﻿// BoostIPCServer.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <boost/interprocess/shared_memory_object.hpp>
+#include <boost/interprocess/mapped_region.hpp>
+
+int main()
+{
+	using namespace boost::interprocess;
+	try {
+		shared_memory_object shdmem{ open_or_create, "Boost", read_write };
+		std::string payload = "Hello, message from IPC server";
+
+		shdmem.truncate(payload.size());
+		mapped_region region{ shdmem, read_write };
+		
+		std::memcpy(region.get_address(), payload.c_str(), payload.size());
+
+		
+
+	}
+	catch (interprocess_exception& e) {
+		// .. .  clean up 
+	}
+
+}
